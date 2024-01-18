64fb5cbad677799a49b56f4a41ac731025dcc69e
Vulnorn <mixadrbk31@gmail.com>
2024-01-18 17:13:44 +0300

    Отладка

--- Program.cs
+++ Program.cs
@@ -9,13 +9,127 @@ namespace DepoOOP
     {
         static void Main(string[] args)
         {
-            ControlRoom controlRoom = new ControlRoom();
+            ControlRoom ControlRoom = new ControlRoom();
+
+            ControlRoom.Work();
         }
     }
 
     class ControlRoom
     {
-        protected List<Van> Vans = new List<Van>();
+        private Train _train;
+        private static Random _random = new Random();
+        public ControlRoom()
+        {
+            IsWork = true;
+            DepartureExpected = false;
+        }
+
+        public bool IsWork { get; private set; }
+        public bool DepartureExpected { get; private set; }
+
+        public void Work()
+        {
+            while (IsWork)
+            {
+                ShowMenu();
+            }
+        }
+
+        private void ShowMenu()
+        {
+            const string CreateDirectionMenu = "1";
+            const string SendTrainMenu = "2";
+            const string ExitMenu = "3";
+
+            Console.Clear();
+            Console.WriteLine("Добро пожаловать");
+            Console.WriteLine($"Выберите пункт в меню:");
+            Console.WriteLine($"{CreateDirectionMenu} - Создать направление");
+            Console.WriteLine($"{SendTrainMenu} - Отправить поезд");
+            Console.WriteLine($"{ExitMenu} - Выход");
+
+            string userInput = Console.ReadLine();
+
+            switch (userInput)
+            {
+                case CreateDirectionMenu:
+                    CreateDirection();
+                    break;
+
+                case SendTrainMenu:
+                    SendTrain();
+                    break;
+
+                case ExitMenu:
+                    Exit();
+                    break;
+
+                default:
+                    Console.WriteLine("Ошибка ввода команды.");
+                    Console.ReadKey();
+                    break;
+            }
+        }
+
+        private void CreateDirection()
+        {
+            if (DepartureExpected == true)
+            {
+                Console.WriteLine("Направление создано ожидается отправка поезда");
+            }
+            else
+            {
+                TryGetDirectioin();
+            }
+
+            Console.ReadKey();
+        }
+
+        private bool TryGetDirectioin()
+        {
+            int lowerLimitRandom = 300;
+            int upperLimitRandom = 1000;
+            Console.Clear();
+            Console.WriteLine("Введите пункт отправление");
+            string departurePoint = Console.ReadLine();
+
+            Console.WriteLine("Введите пункт прибытия");
+            string arrivaPoint = Console.ReadLine();
+
+            int passengers = _random.Next(lowerLimitRandom, upperLimitRandom);
+            _train = new Train(passengers);
+
+            Console.WriteLine($"Вы создали направление {departurePoint} - {arrivaPoint} ожидается поезд на {passengers} пассажиров");
+
+            return DepartureExpected = true;
+        }
+
+        private void SendTrain()
+        {
+            if (DepartureExpected == false)
+            {
+                Console.WriteLine("Нет поезда для отправки.");
+            }
+            else
+            {
+                TryGetTrain();
+            }
+
+            Console.ReadKey();
+        }
+
+        private  bool TryGetTrain()
+        {
+            _train.CreateNew();
+            return DepartureExpected = false;
+        }
+
+
+        private void Exit()
+        {
+            IsWork = false;
+        }
     }
 
     class Train
@@ -26,7 +140,6 @@ namespace DepoOOP
         public Train(int passengers)
         {
             Passengers = passengers;
-
         }
 
         public int Passengers { get; private set; }
@@ -39,9 +152,12 @@ namespace DepoOOP
             while (Passengers > 0)
             {
                 Console.WriteLine($"Разместите {Passengers} по вогонам выбрав доступный из списка.");
+                Complete();
 
             }
 
+            Console.WriteLine("Поезд укомплектован и отправлен по текущему направлению");
+            Console.Clear();
         }
 
         private void Complete()
@@ -50,8 +166,6 @@ namespace DepoOOP
                 Console.WriteLine("Не было добавлено вагона");
             else
                 Console.WriteLine("Вы добавили новый вагон");
-
-            Console.ReadKey();
         }
 
         private bool TryGetVan()
@@ -66,9 +180,12 @@ namespace DepoOOP
             return true;
         }
 
-        private void AccommodatePassengers(int numberVan )
+        private void AccommodatePassengers(int numberVan)
         {
-            _vans[numberVan].
+            Passengers = Passengers - _vans[numberVan].Seating;
+
+            if (Passengers < 0)
+                Passengers = 0;
         }
 
         private bool GetNumberVan(out int numberVan)
@@ -80,18 +197,18 @@ namespace DepoOOP
             return true;
         }
 
-        private List<Van> CreateVans ()
+        private List<Van> CreateVans()
         {
             int randomIndex;
             int minimumNumberSeats = 40;
             int maximumNumberSeats = 201;
-            List<Van > vans = new List<Van>();
+            List<Van> vans = new List<Van>();
             string[] name = new string[] { "Купе", "Плацкарт", "Сидачие", "2х этажный плацкарт" };
 
-            for (int i = 0; i<name.Length; i++)
+            for (int i = 0; i < name.Length; i++)
             {
-                randomIndex = _random.Next(minimumNumberSeats,maximumNumberSeats);
-                vans.Add(new Van(name[i],randomIndex));
+                randomIndex = _random.Next(minimumNumberSeats, maximumNumberSeats);
+                vans.Add(new Van(name[i], randomIndex));
             }
 
             return vans;
@@ -116,7 +233,7 @@ namespace DepoOOP
         }
     }
 
-     class Van
+    class Van
     {
         public Van(string name, int seating)
         {
@@ -129,7 +246,7 @@ namespace DepoOOP
 
         public void ShowInfo()
         {
-            Console.Write($"Вагон {Name}, имеет {Seating} посадочных мест");
+            Console.WriteLine($"Вагон {Name}, имеет {Seating} посадочных мест");
         }
     }
 
